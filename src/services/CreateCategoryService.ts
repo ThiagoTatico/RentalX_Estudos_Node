d8fd4173e8ba8e89d970c8e50322b6f250109725
Ariun <thiagotatico007@gmail.com>
2022-06-13 16:57:35 -0300

    Changes

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,19 +1,22 @@
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
 interface IResquest {
   name: string;
   description: string;
 }
 
 class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
   execute({ name, description }: IResquest) {
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists) {
       throw new Error('Category already exists !!!');
     }
 
-    categoriesRepository.create({ name, description });
+    this.categoriesRepository.create({ name, description });
   }
-
 }
 
 export { CreateCategoryService };
