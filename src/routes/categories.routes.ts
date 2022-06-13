2254e7837438fbe977fc58bc1e60d7468b22b505
Ariun <thiagotatico007@gmail.com>
2022-06-13 17:52:42 -0300

    Changes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { PostgresCategoriesRepository } from '../repositories/PostgresCategoriesRepository';
 import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = new PostgresCategoriesRepository();
 const createCategoryService = new CreateCategoryService(categoriesRepository);
 
 categoriesRoutes.post('/', (req, res) => {
