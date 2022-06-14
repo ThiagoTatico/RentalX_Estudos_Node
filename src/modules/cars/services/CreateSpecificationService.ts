f38c2119a99833619a1ca5b976efd7dd3e8857d4
Ariun <thiagotatico007@gmail.com>
2022-06-14 11:05:02 -0300

    Refactoring

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,18 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateCategoryService {
-  execute() {
-    console.log('todo');
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
 
