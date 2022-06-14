104f56bef946f39962656303ca17d8d2baacc9fa
Ariun <thiagotatico007@gmail.com>
2022-06-14 17:13:06 -0300

    Refactoring

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryUseCase } from '../modules/cars/useCases/createCategory/CreateCategoryUseCase';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
-
+  return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get('/', (req, res) => {
-  const all = categoriesRepository.list();
-
-  return res.json({ all });
+  
 });
 
 export { categoriesRoutes };
