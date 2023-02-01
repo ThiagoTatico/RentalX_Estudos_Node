5b21633c95a8269e30b1e3cd5d1ae4e7401e0c53
Ariun <thiagotatico007@gmail.com>
2023-02-01 15:19:33 -0300

    Add authentication with jwt

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayLoad {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) throw new AppError('Token Missing', 401);
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      'b00a48dd907636259c8cc16e80467be2',
+    ) as IPayLoad;
+
+    const userRepository = new UsersRepository();
+    const user = await userRepository.findById(user_id);
+
+    if (!user) throw new AppError('User does not exists', 401);
+
+    next();
+  } catch {
+    throw new AppError('Invalid Token', 401);
+  }
+}
