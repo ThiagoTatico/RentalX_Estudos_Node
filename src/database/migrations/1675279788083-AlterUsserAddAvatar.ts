65f43a9e1acd0e856c72b7d5dc8c5f24244686cf
Ariun <thiagotatico007@gmail.com>
2023-02-02 10:33:42 -0300

    refactoring

---
+++ src/database/migrations/1675279788083-AlterUsserAddAvatar.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AlterUsserAddAvatar1675279788083 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'avatar');
+  }
+}
